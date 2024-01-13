eabe4c0bf1364d32561c379063c8c7d5b08204df
sarina aghamohammad kashi <142325292+sarina97@users.noreply.github.com>
2024-01-13 15:21:01 +0330

    Update OrganisationServiceImpl.java
    OrganizationServiceImp

--- src/main/java/com/mftplus/automation/service/impl/OrganisationServiceImpl.java
+++ src/main/java/com/mftplus/automation/service/impl/OrganisationServiceImpl.java
@@ -5,19 +5,18 @@ import com.mftplus.automation.service.OrganisationService;
 import jakarta.enterprise.context.SessionScoped;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
-import jakarta.persistence.Transient;
-
+import jakarta.transaction.Transactional;
 import java.io.Serializable;
 import java.util.List;
 import java.util.Optional;
 
 @SessionScoped
-public class OrganisationServiceImpl implements OrganisationService, Serializable {
+public class OrganisationService implements OrganisationServiceImp, Serializable {
 
     @PersistenceContext(unitName = "jk")
     private EntityManager entityManager;
 
-    @Transient
+    @Transactional
     @Override
     public void save(Organisation organisation) throws Exception {
         entityManager.persist(organisation);
@@ -25,27 +24,47 @@ public class OrganisationServiceImpl implements OrganisationService, Serializabl
 
     }
     @Override
+    @Transactional
     public void edit(Organisation organisation) throws Exception {
+        entityManager.merge(organisation);
 
     }
 
     @Override
+    @Transactional
     public void remove(Organisation organisation) throws Exception {
+         organisation = entityManager.find(Organisation.class, organisation.getId());
+        organisation.setDeleted(true);
+        entityManager.merge(organisation);
 
     }
 
     @Override
     public void removeById(Long id) throws Exception {
+        Organisation organisation = entityManager.find(Organisation.class, id);
+        organisation.setDeleted(true);
+        entityManager.merge(organisation);
+
 
     }
 
     @Override
     public List<Organisation> findAll() throws Exception {
-        return null;
+        TypedQuery<Organisation> query = entityManager.createQuery("select oo from organisationEntity oo", Organisation.class);
+        return query.getResultList();
     }
 
     @Override
     public Optional<Organisation> findById(Long id) throws Exception {
-        return Optional.empty();
+        return Optional.ofNullable(entityManager.find(Organisation.class, id));
     }
+    public Optional<Organisation> findByName(String name)throws Exception{
+        TypedQuery<Organisation> query = (TypedQuery<Organisation>) entityManager.createQuery("select s from organisationEntity  s where s.name = :name");
+        query.setParameter("name", name);
+        List<Organisation> result = query.getResultList();
+        return Optional.ofNullable((result.isEmpty()) ? null : result.get(0));
+    }
+
+
+
 }
