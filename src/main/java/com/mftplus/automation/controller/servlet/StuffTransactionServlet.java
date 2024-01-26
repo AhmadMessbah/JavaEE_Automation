7c0f89e757b1d4c31b858ebfabf9a3de7e462ac5
farhad <farhadghasemi.official@gmail.com>
2024-01-26 12:42:20 +0330

    farhad ghasemi servlet

--- src/main/java/com/mftplus/automation/controller/servlet/StuffTransactionServlet.java
+++ src/main/java/com/mftplus/automation/controller/servlet/StuffTransactionServlet.java
@@ -1,7 +1,13 @@
 package com.mftplus.automation.controller.servlet;
 
+import com.mftplus.automation.model.Section;
+import com.mftplus.automation.model.Stuff;
 import com.mftplus.automation.model.StuffTransaction;
+import com.mftplus.automation.model.User;
+import com.mftplus.automation.service.impl.SectionServiceImpl;
+import com.mftplus.automation.service.impl.StuffServiceImpl;
 import com.mftplus.automation.service.impl.StuffTransactionServiceImpl;
+import com.mftplus.automation.service.impl.UserServiceImpl;
 import jakarta.inject.Inject;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
@@ -12,8 +18,8 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.extern.slf4j.Slf4j;
 
-import javax.xml.registry.infomodel.User;
 import java.io.IOException;
+import java.util.Optional;
 
 @Slf4j
 @WebServlet(name = "StuffTransactionServlet", urlPatterns = "/StuffTransaction.do")
@@ -25,17 +31,26 @@ public class StuffTransactionServlet extends HttpServlet {
 
     @Inject
     private StuffTransactionServiceImpl stuffTransactionService;
+    @Inject
+    private UserServiceImpl userService;
+    @Inject
+    private SectionServiceImpl sectionService;
+    @Inject
+    private StuffServiceImpl stuffService;
 
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         log.info("Stuff Transaction Servlet-post");
         try {
+            Optional<User> user=userService.findByUsername(req.getParameter("stuffTransactionUser"));
+            Optional<Section>section=sectionService.findByTitle(req.getParameter("stuffTransactionSection"));
+            Optional<Stuff>stuff=stuffService.findByName(req.getParameter("stuffTransactionStuff"));
             StuffTransaction stuffTransaction=StuffTransaction
                     .builder()
-//                    .user()
-//                    .section()
-//                    .stuff()
+                    .user(user.get())
+                    .section(section.get())
+                    .stuff(stuff.get())
 //                    .transactionType()
                     .build();
             stuffTransactionService.save(stuffTransaction);
