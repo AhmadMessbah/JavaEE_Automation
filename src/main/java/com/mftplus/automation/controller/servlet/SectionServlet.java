451a9ef8607fa0a6fc8aa9c4ff7369e9cfd55d85
sarina97 <sabinadavila020@gmail.com>
2024-01-26 13:48:12 +0330

    section

--- src/main/java/com/mftplus/automation/controller/servlet/SectionServlet.java
+++ src/main/java/com/mftplus/automation/controller/servlet/SectionServlet.java
@@ -6,6 +6,7 @@ import com.mftplus.automation.model.User;
 import com.mftplus.automation.model.enums.FinancialDocType;
 import com.mftplus.automation.service.OrganisationService;
 import com.mftplus.automation.service.SectionService;
+import com.mftplus.automation.service.impl.OrganisationServiceImpl;
 import com.mftplus.automation.service.impl.SectionServiceImpl;
 import jakarta.inject.Inject;
 import jakarta.persistence.EntityManager;
@@ -15,18 +16,19 @@ import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
 
 import java.io.IOException;
 import java.util.Arrays;
 import java.util.List;
 
 @WebServlet(urlPatterns = "/section.do")
+@Slf4j
 public class SectionServlet extends HttpServlet {
-    @PersistenceContext(unitName = "automation")
-    private EntityManager entityManager;
-
     @Inject
     private SectionServiceImpl service;
+    @Inject
+    private OrganisationServiceImpl organisationService;
     @Inject
     private Section section;
 
@@ -34,9 +36,10 @@ public class SectionServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         try {
-            req.getRequestDispatcher("/jsp/section.jsp").forward(req,resp);
-//            req.getSession().setAttribute("org", OrganisationService);
+            /*req.getSession().setAttribute("org", organisationService.findByName());*/
             req.getSession().setAttribute("sectionList", service.findAll());
+            req.getRequestDispatcher("/jsp/section.jsp").forward(req,resp);
+
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
@@ -44,24 +47,26 @@ public class SectionServlet extends HttpServlet {
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        String duty=req.getParameter("duty");
         String title=req.getParameter("title");
+        String duty=req.getParameter("duty");
         String phoneNumber=req.getParameter("phoneNumber");
 
-        req.getRequestDispatcher("/section.jsp");
+        req.getRequestDispatcher("/jsp/section.jsp");
 
         try {
             section=Section.builder()
-                    .duty(duty).title(title).phoneNumber(phoneNumber).build();
+                    .duty(duty)
+                    .title(title)
+                    .phoneNumber(phoneNumber).build();
+
 
             service.save(section);
-
-
+            log.info("Section Save");
+            resp.sendRedirect("/section.do");
         } catch (Exception e) {
+            log.info(e.getMessage());
             throw new RuntimeException(e);
         }
-
-
     }
 
 }
