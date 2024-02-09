697bbf80aa119d6fa7b6fc7467dbc10d0f7f1260
royazargar <roya.zargar1992@gmail.com>
2024-02-09 18:21:34 +0330

    user updated- financial team

--- src/main/java/com/mftplus/automation/model/User.java
+++ src/main/java/com/mftplus/automation/model/User.java
@@ -3,7 +3,6 @@ package com.mftplus.automation.model;
 import com.mftplus.automation.model.enums.Role;
 import jakarta.enterprise.context.RequestScoped;
 import jakarta.persistence.*;
-import jakarta.validation.constraints.Pattern;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -22,16 +21,16 @@ import lombok.experimental.SuperBuilder;
 @RequestScoped
 public class User extends Base{
     @Id
-    @SequenceGenerator(name = "userSeq", sequenceName = "user_seq")
+    @SequenceGenerator(name = "userSeq", sequenceName = "user_seq", initialValue = 1, allocationSize = 1)
     @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "userSeq")
     @Column(name = "id", nullable = false)
     private long id;
 
-    @Pattern(regexp = "^[a-zA-Z\\s]{5,15}$", message = "Invalid Username")
+//    @Pattern(regexp = "^[a-zA-Z\\s]{5,15}$", message = "Invalid Username")
     @Column(name = "u_name", length = 15)
     private String username;
 
-    @Pattern(regexp = "^[a-zA-Z\\s]{8,20}$", message = "Invalid Password")
+//    @Pattern(regexp = "^[a-zA-Z\\s]{8,20}$", message = "Invalid Password")
     @Column(name = "u_pass", length = 20)
     private String password;
 
