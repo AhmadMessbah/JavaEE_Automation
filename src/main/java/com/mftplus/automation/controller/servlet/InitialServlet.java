554a3f7a4855b583985729ed030ab731ee8e0a67
kurromii <ahadzadeparnian@gmail.com>
2024-03-26 05:21:21 +0330

    InitialServlet and RolesServlet added - edari

---
+++ src/main/java/com/mftplus/automation/controller/servlet/InitialServlet.java
@@ -0,0 +1,98 @@
+package com.mftplus.automation.controller.servlet;
+
+import com.mftplus.automation.model.CompositeKey;
+import com.mftplus.automation.model.Roles;
+import com.mftplus.automation.model.User;
+import com.mftplus.automation.service.impl.RolesServiceImpl;
+import com.mftplus.automation.service.impl.UserServiceImpl;
+import jakarta.inject.Inject;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
+
+import java.io.IOException;
+
+@Slf4j
+public class InitialServlet extends HttpServlet {
+    @Inject
+    private UserServiceImpl userService;
+    @Inject
+    private RolesServiceImpl rolesService;
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        try {
+            log.info("InitialServlet - Get");
+            req.getRequestDispatcher("/index.jsp").forward(req,resp);
+        } catch (Exception e) {
+            log.error(e.getMessage());
+            throw new RuntimeException(e);
+        }
+    }
+
+   @Override
+    public void init() throws ServletException {
+        log.info("InitialServlet - Init");
+        super.init();
+//creating admin-admin123 and user-user123
+        try {
+            User user =
+                    User
+                            .builder()
+                            .username("admin")
+                            .password("admin123")
+                            .build();
+            if (userService.findByUsername("admin").isEmpty()){
+            userService.save(user);
+            log.info("admin user saved");
+            }
+            CompositeKey compositeKey =
+                    CompositeKey
+                            .builder()
+                            .roleName("admin")
+                            .user(user)
+                            .build();
+            Roles role =
+                    Roles
+                            .builder()
+                            .compositeKey(compositeKey)
+                            .build();
+            if (rolesService.findById(compositeKey).isEmpty()){
+               rolesService.save(role);
+               log.info("admin role saved");
+            }
+
+            User user1 =
+                    User
+                            .builder()
+                            .username("user")
+                            .password("user123")
+                            .build();
+            if (userService.findByUsername("user").isEmpty()){
+                userService.save(user1);
+                log.info("user saved");
+            }
+            CompositeKey compositeKey1 =
+                    CompositeKey
+                            .builder()
+                            .roleName("user")
+                            .user(user1)
+                            .build();
+            Roles role1 =
+                    Roles
+                            .builder()
+                            .compositeKey(compositeKey1)
+                            .build();
+            if (rolesService.findById(compositeKey1).isEmpty()){
+                rolesService.save(role1);
+                log.info("user role saved");
+            }
+
+        } catch (Exception e) {
+            log.error(e.getMessage());
+            throw new RuntimeException(e);
+        }
+    }
+}
---
+++ src/main/java/com/mftplus/automation/controller/servlet/RolesServlet.java
@@ -0,0 +1,76 @@
+package com.mftplus.automation.controller.servlet;
+
+import com.mftplus.automation.model.CompositeKey;
+import com.mftplus.automation.model.Roles;
+import com.mftplus.automation.model.User;
+import com.mftplus.automation.service.impl.RolesServiceImpl;
+import com.mftplus.automation.service.impl.UserServiceImpl;
+import jakarta.inject.Inject;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
+
+import java.io.IOException;
+import java.util.Optional;
+
+@Slf4j
+@WebServlet(urlPatterns = "/roles.do")
+public class RolesServlet extends HttpServlet {
+    @Inject
+    private RolesServiceImpl rolesService;
+    @Inject
+    private UserServiceImpl userService;
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        log.info("RolesServlet - Get");
+
+        try {
+            //todo
+//            req.getSession().setAttribute("roleTypes", Arrays.asList(Role.values()));
+            req.getSession().setAttribute("rolesList", rolesService.findAll());
+            req.getRequestDispatcher("/roles.jsp").forward(req, resp);
+        } catch (Exception e) {
+            log.error(e.getMessage());
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        log.info("RolesServlet - Post");
+        try {
+            String roleName = req.getParameter("roleName");
+            String username = req.getParameter("username");
+
+            if (username != null){
+                Optional<User> user = userService.findByUsername(username);
+                System.out.println(user);
+
+                if (user.isPresent()){
+                    CompositeKey compositeKey =
+                            CompositeKey
+                                    .builder()
+                                    .roleName(roleName)
+                                    .user(user.get())
+                                    .build();
+                    System.out.println(compositeKey);
+                    Roles role =
+                            Roles
+                                    .builder()
+                                    .compositeKey(compositeKey)
+                                    .build();
+                    System.out.println(role);
+                    rolesService.save(role);
+                    log.info("RolesServlet - role saved");
+                    resp.sendRedirect("/roles.do");
+                }
+            }
+        } catch (Exception e) {
+            log.error(e.getMessage());
+            throw new RuntimeException(e);
+        }
+    }
+}
